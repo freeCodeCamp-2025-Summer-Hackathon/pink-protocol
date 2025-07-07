6e045d2dbe89964b45fbef26d9399aca1c5d3489
afredoherculano <alfredo.hsh@gmail.com>
2025-07-07 16:20:24 -0300

    feat: added main.py and basic schemas

---
+++ backend/schemas.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
+
+
+# User related schemas
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+
+# Post related schemas
+class PostBase(BaseModel):
+    title: str
+    description: str | None = None
+    published: bool = True
+
+class PostCreate(PostBase):
+    pass
+
+class PostResponse(PostBase):
+    id: int
+    artist_id: int
+    artist: UserResponse
+    created_at: datetime
