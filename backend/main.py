fde13b6c68fafbc5712f3a88f6838f791c80befd
Cmolloy36 <cdmolloy36@gmail.com>
2025-07-07 23:41:49 -0400

    added backend skeleton; example endpoints, crud functions, helper functions, and database models; created requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,40 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import schemas, crud_users
+from .database import get_session
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "ArtHive is buzzing!"}
+
+# Users
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def get_user(user_id: int, session: Session = Depends(get_session)):
+    user = crud_users.get_user(session=session, user_id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail=f"a user with this id does not exist")
+    return user
+
+@app.get("/users", response_model=list[schemas.User])
+def get_users(skip: int = 0, limit: int = 10, session: Session = Depends(get_session)):
+    users = crud_users.get_users(session=session, skip=skip, limit=limit)
+    # if users is []: # TODO: determine if we want to raise exception if no users exist, or just return empty list
+    #     ...
+    return users
+
+@app.post("/users",response_model=schemas.User)
+def post_student(user: schemas.UserCreate, session: Session = Depends(get_session)):
+    user, err = crud_users.post_user(session=session, user=user)
+    if err is not None:
+        raise HTTPException(status_code=404, detail=f"unable to add user: {err}")
+    return user
+
+@app.delete("/users/{user_id}", response_model=str)
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    user_name, user_id, err = crud_users.delete_user(session=session, user_id=user_id)
+    if err is not None:
+        raise HTTPException(status_code=404, detail=f"error: {err}")
+    return f"{user_name} (user_id: {user_id}) has been deleted"
---
+++ backend/models.py
@@ -0,0 +1,44 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from datetime import datetime
+from typing import List, Optional
+
+class Base(sa.orm.DeclarativeBase):
+    pass
+
+# TODO: add additional tables (collections, likes, comments)
+# TODO: update model attributes based on discussion about initial required params
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(sa.TIMESTAMP, default=sa.func.now())
+    updated_at: Mapped[datetime] = mapped_column(sa.TIMESTAMP, default=sa.func.now())
+    password_hash: Mapped[str] = mapped_column(nullable=False)
+    # type: Mapped[str] = mapped_column(nullable=False) # also important for joined table inheritance, not necessary otherwise
+
+    # # This is important for joined table inheritance if we were to support different types of users (user, admin, etc.)
+    # __mapper_args__ = {
+    #     "polymorphic_identity": "user",
+    #     "polymorphic_on": "type",
+    #     "with_polymorphic": "*",
+    # }
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id}, name={self.name}, email={self.email}, type={self.type})"
+    
+class Post(Base):
+    __tablename__ = "posts"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    caption: Mapped[str] = mapped_column(nullable=False)
+    created_by: Mapped[int] = mapped_column(sa.ForeignKey("users.id", ondelete="CASCADE"))
+    # content # TODO: Figure out how we represent a post's content. If it's a link, it can be stored here. If raw data...
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id}, name={self.name}, created_by={self.created_by}, caption={self.caption})"
