424aac50c6d4f94f0a88195935f031f1979b648b
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2025-07-09 22:17:13 -0300

    chore(lint): set up Prettier & ESLint flat config
    - install dependencies and plugins
    - add prettier.config.cjs with single-quote, no-semi, tailwind plugin
    - update package.json scripts: format & lint commands
    
    # Conflicts:
    #	frontend/package-lock.json

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,28 +1,53 @@
+import { defineConfig } from 'eslint/config'
+import { FlatCompat } from '@eslint/compat'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
-import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import importPlugin from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier'
+import globals from 'globals'
+
+const compat = new FlatCompat({
+  baseDirectory: path.dirname(fileURLToPath(import.meta.url)),
+})
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  { ignores: ['dist'] },
+
+  js.configs.recommended,
+  react.configs.flat.recommended,
+  reactHooks.configs['recommended-latest'],
+  importPlugin.flatConfigs.recommended,
+
+  ...compat.extends('plugin:jsx-a11y/recommended'),
+
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: { jsx: true } },
+      globals: { ...globals.browser },
     },
+    plugins: { prettier },
     rules: {
+      'prettier/prettier': 'error',
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      'react/jsx-sort-props': ['warn', { callbacksLast: true, shorthandLast: true }],
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+        },
+      ],
+      'no-console': 'warn',
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
