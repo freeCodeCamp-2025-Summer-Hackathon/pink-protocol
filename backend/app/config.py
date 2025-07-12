a291f91e6f8899f9a2e5fa4a02270e66cc04c7c7
Cmolloy36 <cdmolloy36@gmail.com>
2025-07-12 17:26:23 -0400

    everything but the db_password is working. I cannot for the life of me figure out why It cannot read the db_password

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,18 +1,43 @@
+import os
+from pathlib import Path
+from typing import Optional
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_hostname: str
     database_port: str
-    database_password: str
     database_name: str
+    database_password: Optional[str] = None
     database_username: str
-
-    # we do not currently use the fields below
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Set password after initialization
+        if not self.database_password:
+            self.database_password = get_db_password()
+
     class Config:
-        env_file = ".env"
+        env_file = "../.env"
+
+def get_db_password():
+    # get docker secret
+    secret_path = Path('/run/secrets/db_password')
+    if secret_path.exists():
+        return secret_path.read_text().strip()
+    
+    # get local password
+    local_path = Path('../db_password.txt')
+    if local_path.exists():
+        return local_path.read_text().strip()
+    
+    # Fall back to environment variable
+    password = os.getenv('DATABASE_PASSWORD', '')
+    if password:
+       return password 
+
+    raise ValueError("check that compose.yaml secret, db_password.txt, or environment variable has password")
 
 settings = Settings()
--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 from .config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
