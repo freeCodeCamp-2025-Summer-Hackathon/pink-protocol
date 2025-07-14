64cd7442a6ec4ba4ee63710da413d061b385096b
afredoherculano <alfredo.hsh@gmail.com>
2025-07-14 20:24:54 -0300

    created relationships between users, posts and collections tables

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
-import ForeignKey
 import list
 import sqlalchemy as sa
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from backend.app.models import collections_posts_association_table
 
 
 class Base(sa.orm.DeclarativeBase):
@@ -18,7 +20,12 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    posts: Mapped[list["Post"]] = relationship("Posts", cascade="all, delete")
+    posts: Mapped[list["Post"]] = relationship(
+        back_populates="creator", cascade="all, delete-orphan"
+    )
+    collections_user: Mapped[list["Collection"]] = relationship(
+        back_populates="owner", cascade="all, delete-orphan"
+    )
     name: Mapped[str] = mapped_column(nullable=False)
     username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
     email: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
@@ -44,6 +51,10 @@ class Post(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    creator: Mapped["User"] = relationship(back_populates="posts")
+    collections_post: Mapped[list["Collection"]] = relationship(
+        secondary=collections_posts_association_table, back_populates="posts"
+    )
     name: Mapped[str] = mapped_column(nullable=False)
     caption: Mapped[str] = mapped_column(nullable=False)
     created_by: Mapped[int] = mapped_column(sa.ForeignKey("users.id", ondelete="CASCADE"))
@@ -56,10 +67,14 @@ class Post(Base):
 
 # Check if each collection can be of posts by different users. Might be a functionality to add later.
 class Collection(Base):
-    __tablename__ = "colletions"
+    __tablename__ = "collections"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    posts: Mapped[list["Post"]] = relationship(
+        secondary=collections_posts_association_table, back_populates="collections_post"
+    )
+    owner: Mapped["User"] = relationship(back_populates="collections_user")
     name: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(
         nullable=False
