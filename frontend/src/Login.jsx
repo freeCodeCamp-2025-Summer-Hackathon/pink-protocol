584a1a0732c963a02cf3f6d76cbf671c0ed03e3e
Victor Ferreira Yonemoto <victor.softwaredesign@gmail.com>
2025-07-19 17:52:02 -0300

    feat: create zod login schema

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -3,3 +3,11 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import { z } from 'zod'
+
+const loginUserSchema = z.object({
+  email: z.email()
+    .nonempty('Email is required!')
+    .email('Invalid email format'),
+  password: z.string()
+    .nonempty('Password is required!'),
+});
