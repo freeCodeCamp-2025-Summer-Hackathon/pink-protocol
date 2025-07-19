a74053516fdabee3a4858e9702792166cb343474
Victor Ferreira Yonemoto <victor.softwaredesign@gmail.com>
2025-07-19 19:18:27 -0300

    chore: add more imports

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,13 +1,14 @@
-import { useForm } from 'react-hook-form';
-
-import { zodResolver } from '@hookform/resolvers/zod'
-
-import { z } from 'zod'
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import { EyeIcon, EyeSlashIcon, SwatchIcon, UserGroupIcon, SparklesIcon } from "@heroicons/react/24/solid"
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { useNavigate, Link } from "react-router-dom"
 
 const loginUserSchema = z.object({
-  email: z.email()
-    .nonempty('Email is required!')
-    .email('Invalid email format'),
-  password: z.string()
-    .nonempty('Password is required!'),
-});
+  email: z.email().nonempty("Email is required!").email("Invalid email format"),
+  password: z.string().nonempty("Password is required!"),
+})
+
+
