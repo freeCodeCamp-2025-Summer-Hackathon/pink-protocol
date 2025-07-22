c417fbb658d9e0a9fa50bab660b77573cf93fcfc
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2025-07-22 00:56:45 -0300

    hotfix(sign-up): correct endpoint

--- frontend/src/auth/SignUp/services/signUpUser.js
+++ frontend/src/auth/SignUp/services/signUpUser.js
@@ -2,7 +2,7 @@ import { apiClient } from '../../../_lib/apiClient.js'
 
 export const signUpUser = (payload) =>
   apiClient
-    .post('/signup', payload, {
+    .post('/users', payload, {
       validateStatus: (s) => [200, 404, 422].includes(s),
     })
     .then(({ data, status }) => ({ data, httpCode: status }))
